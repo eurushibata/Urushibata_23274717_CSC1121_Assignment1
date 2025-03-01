499f1a591e286d42a8a4fb250cbe547f1fda1f12
Emerson Urushibata <emerson.urushibata@sap.com>
2025-03-01 17:10:44 +0000

    Initial commit

---
+++ ca1.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+from bs4 import BeautifulSoup
+
+
+# Reading the data inside the xml
+# file to a variable under the name 
+# data
+
+file_path = '/cranfield-trec-dataset/cran.all.1400.xml'
+with open(file_path, 'r') as f:
+    data = f.read()
+
+# Passing the stored data inside
+# the beautifulsoup parser, storing
+# the returned object 
+Bs_data = BeautifulSoup(data, "xml")
+
+# Finding all instances of tag 
+# `unique`
+b_unique = Bs_data.find_all('unique')
+
+print(b_unique)
+
+# Using find() to extract attributes 
+# of the first instance of the tag
+b_name = Bs_data.find('child', {'name':'Frank'})
+
+print(b_name)
+
+# Extracting the data stored in a
+# specific attribute of the 
+# `child` tag
+value = b_name.get('test')
+
+print(value)
